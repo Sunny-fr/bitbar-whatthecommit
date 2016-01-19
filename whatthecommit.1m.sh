03730d1aa8129746cf404cb2d55fb34c3bc193cc
Alaa El Helw <work@sunny.fr>
2016-01-19 16:08:54 +0100

    Final Commit

---
+++ whatthecommit.1m.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env /usr/local/bin/node
+
+var request = require('request');
+var $ = require('cheerio');
+
+request('http://whatthecommit.com', function (error, response, html) {
+      if (!error && response.statusCode == 200) {
+        var d = $("#content p", html).text() ;
+        console.log('😁 '+d);
+      }
+    });
