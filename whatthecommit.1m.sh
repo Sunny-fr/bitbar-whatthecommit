9a2ce1d6f5d75d3bf08d1911505b4eacb1ad7073
Alaa El Helw <work@sunny.fr>
2016-01-19 16:20:46 +0100

    misc

--- whatthecommit.1m.sh
+++ whatthecommit.1m.sh
@@ -5,7 +5,7 @@ var $ = require('cheerio');
 
 request('http://whatthecommit.com', function (error, response, html) {
       if (!error && response.statusCode == 200) {
-        var d = $("#content p", html).text() ;
+        var d = $("#content p", html).eq(0).text() ;
         console.log('😁 '+d);
       }
     });
